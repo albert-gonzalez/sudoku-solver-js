3f51cbc6f70918343635848a44d8a3df179bca83
Albert González <albert@mailtrack.io>
2022-02-21 23:09:37 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as wasm from "wasm-solver";
+
 
 function App() {
+  wasm.greet();
   return (
     <div className="App">
       <header className="App-header">
